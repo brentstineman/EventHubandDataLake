7e6a6af0a469f87cbb176cd5a65cb62f066b42e5
Brent Stineman <brent.stineman@microsoft.com>
2016-03-16 09:34:58 -0500

    fixed event processor due to issues found in testing

--- EventProcessor/Program.cs
+++ EventProcessor/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.ServiceBus.Messaging;
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +13,35 @@ namespace EventProcessor
     {
         private static EventProcessorHost myProcessorHost;
 
+        private static readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private static readonly ManualResetEvent _runCompleteEvent = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
-            myProcessorHost = new EventProcessorHost(EventProcessor.Properties.Settings.Default.eventHubPath, EventProcessor.Properties.Settings.Default.consumerGroupName,
-                EventProcessor.Properties.Settings.Default.eventHubConnectionString, EventProcessor.Properties.Settings.Default.storageConnectionString);
+            NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(EventProcessor.Properties.Settings.Default.eventHubConnectionString);
+            EventHubDescription ehd = namespaceManager.GetEventHub(EventProcessor.Properties.Settings.Default.eventHubPath);
+            namespaceManager.CreateConsumerGroupIfNotExistsAsync(ehd.Path, EventProcessor.Properties.Settings.Default.consumerGroupName);
+
+            myProcessorHost = new EventProcessorHost(Guid.NewGuid().ToString(), EventProcessor.Properties.Settings.Default.eventHubPath, 
+                EventProcessor.Properties.Settings.Default.consumerGroupName, EventProcessor.Properties.Settings.Default.eventHubConnectionString, 
+                EventProcessor.Properties.Settings.Default.storageConnectionString);
 
             myProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>().Wait();
 
-            // never ending loop
-            while (true)
+            RunAsync(_cancellationTokenSource.Token).Wait();
+
+            _cancellationTokenSource.Cancel();
+            _runCompleteEvent.WaitOne();
+
+            // Unregister the processor 
+            myProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+
+        private static async Task RunAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
             {
-                Thread.Sleep(10000);
+                await Task.Delay(1000, cancellationToken);
             }
         }
     }
